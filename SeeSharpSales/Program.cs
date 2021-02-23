6f5bd77d586d96534d853144800844206220c569
Raul Cepeda-Vestly <raul.cepeda-vestly@dnb.no>
2021-02-23 11:44:18 +0100

    Lastet inn kode for csv fil lesing

--- SeeSharpSales/Program.cs
+++ SeeSharpSales/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
 
 namespace SeeSharpSales
 {
@@ -8,7 +11,25 @@ namespace SeeSharpSales
         {
             Console.WriteLine("Welcome to project See Sharp Sales");
 
+            using (var streamReader = new StreamReader("../../Files/SalesRecords.csv"))
+            {
+                using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
+                {
+                    //var records = csvReader.GetRecords<dynamic>().ToList(); 
+                    string value;
+                    //string[] Documents = System.IO.Directory.GetFiles("../../Files/SalesRecords.csv");
 
+                    while (csvReader.Read())
+                    {
+                        for (int i = 0; csvReader.TryGetField(i, out value); i++)
+                        {
+                            Console.WriteLine($"{value}");
+                        }
+
+                        Console.WriteLine();
+                    }
+                }
+            }
         }
     }
 }

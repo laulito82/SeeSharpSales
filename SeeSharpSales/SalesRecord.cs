0dc6da9720fa87d7503064615ca0195d1c126eab
Raul Cepeda-Vestly <raul.cepeda-vestly@dnb.no>
2021-02-23 19:38:47 +0100

    leker meg med kode

--- SeeSharpSales/SalesRecord.cs
+++ SeeSharpSales/SalesRecord.cs
@@ -4,22 +4,22 @@ using System.Linq;
 namespace SeeSharpSales
 {
     #region enums
-    public enum SalesChannel 
+    public enum SalesChannel
     {
         Online, Offline
     }
 
     public enum OrderPriority
     {
-        C, H, L, M 
+        C, H, L, M
     }
     #endregion
 
 
     public class SalesRecord
     {
-        
-        
+
+
         public string Region { get; set; }
         public string Country { get; set; }
         public string ItemType { get; set; }
@@ -31,14 +31,14 @@ namespace SeeSharpSales
         public int UnitsSold { get; set; }
         public double UnitPrice { get; set; }
         public double UnitCost { get; set; }
-        public double TotalRevenue { get; set; } 
+        public double TotalRevenue { get; set; }
         public double TotalCost { get; set; }
         public double TotalProfit { get; set; }
-        
+
 
         public SalesRecord()
         {
-            
+
         }
 
         public bool isRegion(string regionInn)
@@ -65,9 +65,27 @@ namespace SeeSharpSales
             return false;
         }
 
-        //public int totalUnitsSold(int totalSales)
-        //{
-        //    return totalSales = UnitsSold.Sum;
-        //}
+        /*
+        public int totalUnitsSold(int totalSales)
+        {
+            //return totalSales = UnitsSold.Sum();
+            foreach(totalSales in UnitsSold)
+            {
+                sum += totalS;
+            }
+        }
+        
+        foreach (var sold in  )
+
+        {
+        var numbers = new List<int> { 2, 3, 4, 5, 6 };
+
+        int sum = 0;
+
+        sum = numbers.Sum();
+            Console.WriteLine(sum);
+        }
+        */
+
     }
 }

f3d72d6a3c7abe928fe674f7ce6dc9ca8e0fc9aa
Anne Karen Aanonli <Anne.Karen.Aanonli@dnb.no>
2021-02-23 15:01:28 +0100

    rensket opp litt på navnene - public er implisit når det er interface

--- SeeSharpSales/ISeeSharpSales.cs
+++ SeeSharpSales/ISeeSharpSales.cs
@@ -22,20 +22,33 @@ namespace SeeSharpSales
         void CorrectFileName();
 
         //2 coulmns we new in the file we want to use
-        public string Region { get; }
-        public string Country { get; }
-        public string ItemType { get; }
-        public SalesChannel SalesChannel { get; }
-        public OrderPriority OrderPriority { get; }
-        public DateTime OrderDate { get; }
-        public int OrderID { get; }
-        public DateTime ShipDate { get; }
-        public int unitsSold { get; }
-        public double unitPrice { get; }
-        public double unitCost { get; }
-        public double totalRevenue { get; }
-        public double totalCost { get; }
-        public double totalProfit { get; }
+        string Region { get; }
+
+        string Country { get; }
+
+        string ItemType { get; }
+
+        SalesChannel SalesChannel { get; }
+
+        OrderPriority OrderPriority { get; }
+
+        DateTime OrderDate { get; }
+
+        int OrderID { get; }
+
+        DateTime ShipDate { get; }
+
+        int unitsSold { get; }
+
+        double unitPrice { get; }
+
+        double unitCost { get; }
+
+        double totalRevenue { get; }
+
+        double totalCost { get; }
+
+        double totalProfit { get; }
 
     }
 }
